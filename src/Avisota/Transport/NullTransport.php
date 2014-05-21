c0824294a75342358036b5f8275aad23e59ce22f
Tristan Lins <tristan.lins@bit3.de>
2014-05-21 16:22:36 +0200

    Add null transport implementation.

---
+++ src/Avisota/Transport/NullTransport.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ *
+ * PHP Version 5.3
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota-core
+ * @license    LGPL-3.0+
+ * @link       http://avisota.org
+ */
+
+namespace Avisota\Transport;
+
+use Avisota\Message\MessageInterface;
+
+/**
+ * Transport that simply does nothing.
+ *
+ * @package avisota-core
+ */
+class NullTransport implements TransportInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function initialise()
+	{
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function send(MessageInterface $message)
+	{
+		return new TransportStatus($message, 1);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function flush()
+	{
+	}
+}
