e32b54a722955f4d7ca4e190ab3d72a978556275
Tristan Lins <tristan.lins@bit3.de>
2014-08-22 11:29:16 +0200

    Add union recipient source.

---
+++ src/Avisota/RecipientSource/Union.php
@@ -0,0 +1,229 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ *
+ * PHP Version 5.3
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota-core
+ * @license    LGPL-3.0+
+ * @link       http://avisota.org
+ */
+
+namespace Avisota\RecipientSource;
+
+/**
+ * Combine one or more recipient sources.
+ *
+ * @package avisota-core
+ */
+class Union implements RecipientSourceInterface
+{
+
+	/**
+	 * @var RecipientSourceInterface[]
+	 */
+	private $recipientSources;
+
+	/**
+	 * @var bool
+	 */
+	private $clean = false;
+
+	/**
+	 * @param array|\Traversable|RecipientSourceInterface[] $fileData
+	 */
+	public function __construct($recipientSources = array(), $clean = false)
+	{
+		$this->setRecipientSources($recipientSources);
+		$this->clean = (bool) $clean;
+	}
+
+	/**
+	 * @return RecipientSourceInterface[]
+	 */
+	public function getRecipientSources()
+	{
+		return array_values($this->recipientSources);
+	}
+
+	/**
+	 * @param array|\Traversable|RecipientSourceInterface[] $recipientSources
+	 *
+	 * @return static
+	 */
+	public function setRecipientSources($recipientSources)
+	{
+		$this->recipientSources = array();
+		$this->addRecipientSources($recipientSources);
+		return $this;
+	}
+
+	/**
+	 * @param array|\Traversable|RecipientSourceInterface[] $recipientSources
+	 *
+	 * @return static
+	 */
+	public function addRecipientSources($recipientSources)
+	{
+		foreach ($recipientSources as $recipientSource) {
+			$this->addRecipientSource($recipientSource);
+		}
+		return $this;
+	}
+
+	/**
+	 * @param RecipientSourceInterface $recipientSource
+	 *
+	 * @return static
+	 */
+	public function addRecipientSource(RecipientSourceInterface $recipientSource)
+	{
+		$hash                          = spl_object_hash($recipientSource);
+		$this->recipientSources[$hash] = $recipientSource;
+		return $this;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isClean()
+	{
+		return $this->clean;
+	}
+
+	/**
+	 * @param boolean $clean
+	 *
+	 * @return static
+	 */
+	public function setClean($clean)
+	{
+		$this->clean = $clean;
+		return $this;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function countRecipients()
+	{
+		$count = 0;
+
+		if ($this->isClean()) {
+			$emails = array();
+
+			foreach ($this->recipientSources as $recipientSource) {
+				$offset = 0;
+
+				do {
+					$recipients = $recipientSource->getRecipients(10, $offset);
+
+					foreach ($recipients as $recipient) {
+						if (!in_array($recipient->getEmail(), $emails)) {
+							$emails[] = $recipient->getEmail();
+						}
+						unset($recipient);
+					}
+
+					$count = count($recipients);
+					$offset += 10;
+					unset($recipients);
+				}
+				while ($count > 0);
+			}
+
+			$count = count($emails);
+		}
+		else {
+			foreach ($this->recipientSources as $recipientSource) {
+				$count += $recipientSource->countRecipients();
+			}
+		}
+
+		return $count;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getRecipients($limit = null, $offset = null)
+	{
+		$recipients = array();
+
+		if ($this->isClean()) {
+			$emails = array();
+
+			// duplicate array
+			/** @var RecipientSourceInterface[] $recipientSources */
+			$recipientSources = array_merge($this->recipientSources);
+
+			// we need to walk over all offset and limited recipients
+			if ($limit !== null && $offset !== null) {
+				$limit += $offset;
+			}
+
+			while (
+				count($recipientSources) &&
+				($limit === null || count($recipients) < $limit)
+			) {
+				// get the current recipient source
+				/** @var RecipientSourceInterface $recipientSource */
+				$recipientSource = array_shift($recipientSources);
+
+				// walk over recipients from the current recipient source
+				$localOffset = 0;
+				do {
+					$tempRecipients = $recipientSource->getRecipients(10, $localOffset);
+
+					foreach ($tempRecipients as $recipient) {
+						if (!in_array($recipient->getEmail(), $emails)) {
+							$recipients[] = $recipient;
+							$emails[] = $recipient->getEmail();
+						}
+						unset($recipient);
+
+						if ($limit !== null && count($recipients) >= $limit) {
+							break;
+						}
+					}
+
+					$count = count($tempRecipients);
+					$localOffset += 10;
+					unset($tempRecipients);
+				}
+				while ($count > 0 && ($limit === null || count($recipients) < $limit));
+			}
+
+			if ($offset !== null) {
+				$recipients = array_slice($recipients, $offset);
+			}
+		}
+		else {
+			// duplicate array
+			$recipientSources = array_merge($this->recipientSources);
+
+			while (
+				count($recipientSources) &&
+				($limit === null || count($recipients) < $limit)
+			) {
+				// calculate limit for current recipient source
+				$tempLimit = $limit === null ? null : $limit - count($recipients);
+				// get the current recipient source
+				$recipientSource = array_shift($recipientSources);
+				// fetch the recipients from current recipient source
+				$tempRecipients = $recipientSource->getRecipients($tempLimit, $offset);
+				// merge current with previous recipients
+				$recipients = array_merge($recipients, $tempRecipients);
+
+				if ($offset > 0) {
+					$offset -= empty($tempRecipients) ? $recipientSource->countRecipients() : count($tempRecipients);
+				}
+			}
+		}
+
+		return $recipients;
+	}
+}
---
+++ test/Avisota/Test/RecipientSource/UnionTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ *
+ * PHP Version 5.3
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota-core
+ * @license    LGPL-3.0+
+ * @link       http://avisota.org
+ */
+
+namespace Avisota\Test\RecipientSource;
+
+use Avisota\Recipient\MutableRecipient;
+use Avisota\RecipientSource\CSVFile;
+use Avisota\RecipientSource\Union;
+
+class UnionTest extends CSVFileTest
+{
+	/**
+	 * @return CSVFile
+	 */
+	protected function getUnionRecipientSource($clean) {
+		$unionRecipientSource = new Union();
+		$unionRecipientSource->setClean($clean);
+
+		$unionRecipientSource->addRecipientSource($this->getRecipientSource());
+		$unionRecipientSource->addRecipientSource($this->getRecipientSource());
+
+		return $unionRecipientSource;
+	}
+
+	protected function getUnionRecipients($clean)
+	{
+		$recipients = $this->getRecipients();
+
+		if (!$clean) {
+			$recipients = array_merge($recipients, $this->getRecipients());
+		}
+
+		return $recipients;
+	}
+
+	/**
+	 * @covers Avisota\RecipientSource\CSVFile::countRecipients
+	 */
+	public function testCountRecipients()
+	{
+		foreach (array(true, false) as $clean) {
+			$recipientSource = $this->getUnionRecipientSource($clean);
+			$recipients      = $this->getUnionRecipients($clean);
+
+			$this->assertEquals(count($recipients), $recipientSource->countRecipients());
+		}
+	}
+
+	/**
+	 * @covers Avisota\RecipientSource\CSVFile::getRecipients
+	 */
+	public function testGetRecipients()
+	{
+		foreach (array(true, false) as $clean) {
+			$recipientSource = $this->getUnionRecipientSource($clean);
+			$recipients      = $this->getUnionRecipients($clean);
+
+			// assert complete list
+			$this->assertEquals($recipients, $recipientSource->getRecipients());
+
+			// assert limited list
+			$this->assertEquals(array_slice($recipients, 0, 1), $recipientSource->getRecipients(1));
+
+			// assert offset list
+			$this->assertEquals(array_slice($recipients, 1), $recipientSource->getRecipients(1000, 1));
+
+			// assert go through count
+			$count = $recipientSource->countRecipients();
+
+			for ($offset = 0; $offset < $count; $offset++) {
+				$this->assertEquals(
+					array_slice($recipients, $offset, 1),
+					$recipientSource->getRecipients(1, $offset),
+					'Failed to get recipient at position ' . $offset . ', clean mode is ' . ($clean ? 'on' : 'off')
+				);
+			}
+		}
+	}
+}
